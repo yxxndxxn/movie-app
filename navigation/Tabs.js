d4fc01b5fa2649c602c6d32855703e7d111002bf
yxxndxxn <yxxndxxn@gmail.com>
2025-05-20 00:47:48 +0900

    style: Navigator style 설정

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -2,14 +2,28 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
+import { View, Text } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
+//screenOptions은 모든 screen에 적용할 설정
+//options은 navigator의 단 하나의 화면에 적용할 설정
+
 const Tabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Movies" component={Movies} />
-      <Tab.Screen name="Tv" component={Tv} />
+    <Tab.Navigator initialRouteName="Search">
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          headerRight: () => (
+            <View>
+              <Text>dds</Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen name="Tv" component={Tv} options={{ tabBarBadge: 4 }} />
       <Tab.Screen name="Search" component={Search} />
     </Tab.Navigator>
   );
