17c025a77398742faaf81d335a162648bc622643
yxxndxxn <yxxndxxn@gmail.com>
2025-05-27 23:05:01 +0900

    ✨ feat: swiper 설정

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,16 +1,57 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
 
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
-  navigation: { navigate },
-}) => {
+import Swiper from "react-native-swiper";
+
+const styles = StyleSheet.create({
+  slide1: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#9DD6EB",
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#97CAE5",
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#92BBD9",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+});
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   return (
-    <TouchableOpacity
-      className="p-5 bg-red-500 rounded-lg"
-      onPress={() => navigate("Stack", { screen: "Three" })}
-    >
-      <Text className="text-white text-xl font-bold">Movies</Text>
-    </TouchableOpacity>
+    <ScrollView>
+      <Swiper
+        autoplay={true}
+        showsButtons={false}
+        autoplayTimeout={3.5}
+        loop
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Hello Swiper</Text>
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Beautiful</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+      </Swiper>
+    </ScrollView>
   );
 };
 
